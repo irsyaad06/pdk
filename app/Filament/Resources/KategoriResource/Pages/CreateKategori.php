e7e8f08da4984508a8ac5f78c91dc709c1dd3ec6
irsyaad06 <irsyadfatahilah@gmail.com>
2025-08-13 19:00:02 +0700

    feat : crud kategori

---
+++ app/Filament/Resources/KategoriResource/Pages/CreateKategori.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Resources\KategoriResource\Pages;
+
+use App\Filament\Resources\KategoriResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateKategori extends CreateRecord
+{
+    protected static string $resource = KategoriResource::class;
+
+
+    protected function getRedirectUrl(): string
+    {
+        return KategoriResource::getUrl('index');
+    }
+}
---
+++ app/Models/Kategori.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kategori extends Model
+{
+    use HasFactory;
+
+    protected $table = 'kategori';
+
+    protected $fillable = [
+        'nama',
+        'icon',
+    ];
+
+    /**
+     * Relasi ke Kegiatan
+     */
+    public function kegiatan()
+    {
+        return $this->hasMany(Kegiatan::class);
+    }
+}
